1c99aed160b57237ebb2cf70d47cfa8569f71804
david <david.muelle2@web.de>
2024-03-09 14:42:08 +0100

    Debug

--- packages/privacyhub/src/NeoPixelController.ts
+++ packages/privacyhub/src/NeoPixelController.ts
@@ -187,7 +187,7 @@ export default class NeoPixelController {
                         const saturation = hsvColor.s + saturationDifference * (realElapsed / this.spinnerOptions.rotationDuration);
                         let value = hsvColor.v + valueDifference * (realElapsed / this.spinnerOptions.rotationDuration);
 
-                        if (realElapsed < i * durationPerIndex) {
+                        if (realElapsed < 1 - i * durationPerIndex) {
                             value = value * Math.max(0, 1 - (currentRotation / tailRotationPart));
                         }
                         if (i == 0) {
