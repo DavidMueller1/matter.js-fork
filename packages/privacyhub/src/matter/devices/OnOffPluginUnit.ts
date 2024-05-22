2224e01d2f1a96561c152126e3586083c0d38e45
david <david.muelle2@web.de>
2024-05-22 14:59:02 +0200

    Debug

--- packages/privacyhub/src/matter/devices/OnOffPluginUnit.ts
+++ packages/privacyhub/src/matter/devices/OnOffPluginUnit.ts
@@ -115,7 +115,7 @@ export default class OnOffPluginUnit extends BaseDevice {
                 if (toggle) {
                     onOffCluster.toggle().then(() => {
                         // this.virtualDevice.setOnOffState(state);
-                        // this.updateSocketAndDB();
+                        this.updateSocketAndDB();
                         resolve();
                     }).catch((error) => {
                         this.logger.error(`Failed to toggle OnOff: ${error}`);
@@ -124,7 +124,7 @@ export default class OnOffPluginUnit extends BaseDevice {
                 } else {
                     (state ? onOffCluster.on() : onOffCluster.off()).then(() => {
                         // this.virtualDevice.setOnOffState(state);
-                        // this.updateSocketAndDB();
+                        this.updateSocketAndDB();
                         resolve();
                     }).catch((error) => {
                         this.logger.error(`Failed to set OnOff: ${error}`);
