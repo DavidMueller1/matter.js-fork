8dbf9e29ba76673894a91f9a0446b754677b641f
david <david.muelle2@web.de>
2024-05-27 15:28:00 +0200

    Debug

--- packages/privacyhub/src/app.ts
+++ packages/privacyhub/src/app.ts
@@ -76,32 +76,25 @@ const logger = Logger.get("app");
 
 import os from 'os';
 
-// function getLocalIpAddress(): string | undefined {
-//     const networkInterfaces = os.networkInterfaces();
-//     let localIpAddress: string | undefined;
-//
-//     for (const interfaceName of Object.keys(networkInterfaces)) {
-//         const addresses = networkInterfaces[interfaceName];
-//
-//         if (addresses) {
-//             for (const addressInfo of addresses) {
-//                 if (addressInfo.family === 'IPv4' && !addressInfo.internal) {
-//                     localIpAddress = addressInfo.address;
-//                     break;
-//                 }
-//             }
-//         }
-//
-//         if (localIpAddress) {
-//             break;
-//         }
-//     }
-//
-//     return localIpAddress;
-// }
-//
-// const localIpAddress = getLocalIpAddress();
-console.log('Local IP Addresses:', os.networkInterfaces());
+function getLocalIpAddresses(): string[] {
+    const networkInterfaces = os.networkInterfaces();
+
+    const addresses: string[] = [];
+
+    for (const [_, interfaces] of Object.entries(networkInterfaces)) {
+        if (interfaces === undefined) {
+            continue;
+        }
+        for (const iface of interfaces) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                addresses.push(iface.address);
+            }
+        }
+    }
+
+    return addresses;
+}
+console.log('Local IP Addresses:', getLocalIpAddresses());
 
 
 logger.info(`HCI ID: ${parseInt(process.env.HCI_ID || "0")}`);
